cf4e136cc7a375e4abded4bf7a02887fef8ba89a
BrianMacias <utm19040026@utma.edu.mx>
2023-11-06 16:05:44 -0600

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { CharacterList } from "./components/CharacterList";
-import video from '../src/assets/video/ram.mp4'
+
 
 
 function App() {
   return (
     <main className="bg-accent flex flex-col justify-center items-center">
-      <video
-        autoPlay
-        loop
-        src={video}
-        muted
-        className="w-full h-full object-cover fixed top-0 left-0"
-      />
+      
       <img
-        src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty.png"
-        alt="Rick and Morty"
+        src="https://1000marcas.net/wp-content/uploads/2021/05/Clash-Royale-logo.png"
+        alt="Clash Royale"
         className="h-60"
         style={{ zIndex: 1 }}
       />
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,38 +1,16 @@
 export function Character(character) {
   return (
     <>
-    <div className="card card-side bg-base-100 bg-accent shadow-xl">
-      <figure>
-        <img src={character.image} alt={character.name} className="" />
-      </figure>
-      <div className="card-body">
-        <h3 className="card-title">{character.name}</h3>
-        <p>{`Origin: ${character.origin && character.origin.name}`}</p>
-        <div className="card-actions justify-end">
-        <button
-              className="btn btn-primary hover:underline"
-              onClick={() => document.getElementById("my_modal_3").showModal()}
-            >
-              More
-            </button>
+      <div className="card w-96 bg-base-100 shadow-xl image-full">
+        <figure>
+          <img src={character.image} alt={character.name} className="" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{character.name}</h2>
+          <p>{`Origin: ${character.origin && character.origin.name}`}</p>
         </div>
       </div>
-    </div>
-    <dialog id="my_modal_3" className="modal">
-    <div className="modal-box">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
-          ✕
-        </button>
-      </form>
-      <h3 className="text-lg font-bold">{character.name}</h3>
-      <p className="py-4">{character.status}</p>
-      <p className="py-4">{character.species}</p>
-      <p className="py-4">{character.gender}</p>
-    </div>
-  </dialog>
-  </>
+    </>
   );
 }
 export default Character;
