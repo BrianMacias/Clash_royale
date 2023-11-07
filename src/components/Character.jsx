97f59a7ae75a6000293024de9b802244cec795ee
BrianMacias <utm19040026@utma.edu.mx>
2023-11-06 19:10:06 -0600

    Ultima modificacion

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,10 +1,11 @@
 export function Character(character) {
+  console.log(character)
   return (
+
+    
     <>
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <img src={character.image} alt={character.name} className="" />
-        </figure>
+      <div className="card w-96 bg-primary text-primary-content">
+      
         <div className="card-body">
           <h2 className="card-title">{character.name}</h2>
           <p>{`Origin: ${character.origin && character.origin.name}`}</p>
